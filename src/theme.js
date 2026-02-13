add12795e7d2b9eeb6eed1e90eac89783410ab23
Aman Joshi <amanjoshi16011997@gmail.com>
2026-02-13 14:47:49 +0530

    fix gap

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  fonts: {
+ fonts: {
     heading: `'Playfair Display', serif`,
     body: `'Inter', sans-serif`,
   },
